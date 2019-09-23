5fdd607cd9f4a32c07d3427a8967ca7fecfdba6a
yehonadav <yonadav.barilan@gmail.com>
2019-09-23 06:12:07 +0300

    adding upgrade function

--- qaviton_pip/pip_wrapper.py
+++ qaviton_pip/pip_wrapper.py
@@ -6,6 +6,7 @@ class PIP:
     def __init__(pip): pip.version = pip('--version')
     def __call__(pip, *args): return p(*args)
     def install(pip, *args): return pip('install', *args)
+    def upgrade(pip, *args): return pip('install', *args, '-U')
     def uninstall(pip, *args): return pip('uninstall', *args, '-y')
     def freeze(pip, filename, *args): return pip('freeze', '>', filename, *args)
     def exist(pip, package):
